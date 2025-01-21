63319bb1bb0a80dd4772760aaeb42b72502d4adc
jp kam <jpkamdem@proton.me>
2025-01-21 11:36:05 +0100

    suppression enums, validators utilisés à la place, session controller minors changes

--- app/controllers/session_controller.ts
+++ app/controllers/session_controller.ts
@@ -1,21 +1,15 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { extractErrorMessage } from '../utils.js'
 import User from '#models/user'
-import { emailRegex } from '../enums.js'
 import { loginUserValidation, registerUserValidation } from '#validators/auth'
 
 export default class SessionController {
   async store({ request, response }: HttpContext) {
     try {
       const { email, password } = await request.validateUsing(loginUserValidation)
-      if (!emailRegex.test(email)) {
-        return response.abort({ message: 'Votre email est invalide' })
-      }
 
       const user = await User.verifyCredentials(email, password)
-
       const token = await User.accessTokens.create(user)
-
       return response.status(201).json({ message: 'Connecté', token: token })
     } catch (error: unknown) {
       console.log(error)
--- app/enums.ts
+++
@@ -1,7 +0,0 @@
-export type Role = 'USER' | 'ROLE'
-
-export type Status = 'opérationnel' | 'blessé' | 'suspendu' | 'inconnu'
-
-export type Position = 'gk' | 'def' | 'mf' | 'fw'
-
-export const emailRegex = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/
